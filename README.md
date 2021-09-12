f4eef2fb73932a322561848b127c80db8110cf45
Alessandro Tundo <alessandrotundo94@gmail.com>
2021-09-12 17:56:01 +0200

    Update README and other minor fixes

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# pagerank
-A naive implementation of the PageRank algorithm based on the power method
+# PageRank
+A naive implementation of the PageRank algorithm based on the power method.
+
+## Requirements
+All the required Python packages are listed in the `requirements.txt` file.
+For instance, you can install them using [pip](https://pypi.org/project/pip/) by running the command below:
+
+```shell
+pip install -r requirements.txt
+```
+
+## How to run
+The file `main.py` contains the algorithm implementation and some utility methods.
+The script arguments can be shown by running `python3 main.py --help`. For the sake of simplicity they are reported below:
+
+| Argument | Description | Default value |
+|---|---|---|
+| -h, --help | Show the help message and exit |  |
+| --nodes | The number of nodes of the generated graph | 25 |
+| --edge_probability | The edge probability of the generated graph | 0.125 |
+| --seed | The random seed used during the graph generation and for plots layout | 100 |
+| --dumping_factor | The dumping factor used by the algorithm | 0.85 |
+| --iterations | The number of iterations of the algorithm | 100 |
+
+For instance, you can run `python3 main.py --nodes 50` to generate a graph with 50 nodes while using the default values
+for all the other arguments.
+
+Some informative messages providing details about the current step will appear in the console during the script execution.
+Moreover, the naive algorithm implementation is compared with the [NetworkX](https://networkx.org/documentation/stable/index.html) implementation.
+The results of both the algorithms are reported at the end of the execution along with a plot (only when nodes are <= 50).
+
+## License
+This project is licensed under the GPL v3.0 license. See the [LICENSE](LICENSE) file for details.
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
+#!/usr/bin/env python3
+
 import operator
 import argparse
 import logging
@@ -20,16 +21,16 @@ LOGGER = logging.getLogger(__name__)
 def init_argument_parser():
     parser = argparse.ArgumentParser(description='PageRank')
 
-    parser.add_argument('--nodes', default=50, type=int,
-                        help='Graph nodes number')
-    parser.add_argument('--edge_probability', default=0.025, type=float,
-                        help='Edge probability')
+    parser.add_argument('--nodes', default=25, type=int,
+                        help='The number of nodes of the generated graph')
+    parser.add_argument('--edge_probability', default=0.125, type=float,
+                        help='The edge probability of the generated graph')
     parser.add_argument('--seed', default=100, type=int,
-                        help='Random seed')
+                        help='The random seed used during the graph generation and for plots layout')
     parser.add_argument('--dumping_factor', default=0.85, type=float,
-                        help='Dumping factor')
+                        help='The dumping factor used by the algorithm')
     parser.add_argument('--iterations', default=100, type=int,
-                        help='Number of iterations')
+                        help='The number of iterations of the algorithm')
     return parser
 
 
@@ -51,7 +52,7 @@ def apply_power_method(H, iterations):
     I = I / np.linalg.norm(I)
     initial_eigenvalue = I.T @ H @ I / (I.T @ I)
 
-    LOGGER.info("I = {}".format(initial_eigenvalue))
+    LOGGER.info("Initial I = {}".format(initial_eigenvalue))
 
     old_eigenvalue = initial_eigenvalue
 
@@ -97,22 +98,23 @@ def compare_algorithm(graph, dumping_factor, iterations, I, seed):
     sorted_networkx_pagerank = sorted(pagerank.items(), key=operator.itemgetter(1))
     sorted_naive_pagerank = sorted(naive_pagerank.items(), key=operator.itemgetter(1))
 
-    LOGGER.info("Naive PageRank:")
-    LOGGER.info(sorted_naive_pagerank)
+    LOGGER.info("Naive PageRank results:")
+    LOGGER.info([page[0] for page in sorted_naive_pagerank])
 
-    LOGGER.info("NetworkX PageRank:")
-    LOGGER.info(sorted_networkx_pagerank)
+    LOGGER.info("NetworkX PageRank results:")
+    LOGGER.info([page[0] for page in sorted_networkx_pagerank])
 
-    show_plots(graph, I, I_networkx, seed)
+    if graph.number_of_nodes() <= 50:
+        show_plots(graph, I, I_networkx, seed)
 
 
 def show_plots(G, I, I_networkx, seed):
     fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.set_title("Current implementation")
-    nx.draw(G, ax=ax1, cmap=plt.get_cmap('Reds'), node_color=I, with_labels=True, font_weight='bold',
+    ax1.set_title("Naive implementation")
+    nx.draw(G, ax=ax1, cmap=plt.get_cmap('Blues'), node_color=I, with_labels=True, font_weight='bold',
             pos=nx.spring_layout(G, seed=seed))
     ax2.set_title("NetworkX")
-    nx.draw(G, ax=ax2, cmap=plt.get_cmap('Reds'), node_color=I_networkx, with_labels=True, font_weight='bold',
+    nx.draw(G, ax=ax2, cmap=plt.get_cmap('Blues'), node_color=I_networkx, with_labels=True, font_weight='bold',
             pos=nx.spring_layout(G, seed=seed))
     plt.show()
 
